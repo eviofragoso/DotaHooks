1c61dcb7f1a93663a7018112ec5e64c773e1a938
Evio Fragoso <evio.junior@spezi.com.br>
2021-04-08 18:17:14 -0300

    changed json structure a little

--- pkg/gsi/parser.go
+++ pkg/gsi/parser.go
@@ -1,27 +1,11 @@
 package gsi
 
 type GameState struct {
-	Previously interface{} `json:"previously"`
-	Added      struct {
-		Items struct {
-			Radiant struct {
-				Player0 AddedPlayer `json:"player0"`
-				Player1 AddedPlayer `json:"player1"`
-				Player2 AddedPlayer `json:"player2"`
-				Player3 AddedPlayer `json:"player3"`
-				Player4 AddedPlayer `json:"player4"`
-			} `json:"team2"`
-			Dire struct {
-				Player5 AddedPlayer `json:"player5"`
-				Player6 AddedPlayer `json:"player6"`
-				Player7 AddedPlayer `json:"player7"`
-				Player8 AddedPlayer `json:"player8"`
-				Player9 AddedPlayer `json:"player9"`
-			} `json:"team3"`
-		} `json:"items"`
-	} `json:"added"`
-	Items     struct{} `json:"items"`
-	Buildings struct {
+	Previously *GameState `json:"previously"`
+	Added      *GameState `json:"added"`
+	Items      Items      `json:"items"`
+	Map        Map        `json:"map"`
+	Buildings  struct {
 		Radiant interface{} `json:"radiant"`
 		Dire    interface{} `json:"dire"`
 	} `json:"building"`
@@ -30,28 +14,60 @@ type GameState struct {
 	Player interface{} `json:"player"`
 }
 
-type AddedPlayer struct {
+type Map struct {
+	Name              string `json:"name"`
+	MatchID           int16  `json:"matchid"`
+	Gametime          string `json:"gametime"`
+	Clocktime         string `json:"clocktime"`
+	Daytime           bool   `json:"daytime"`
+	NightstalkerNight bool   `json:"nightstalker_night"`
+	Gamestate         string `json:"gamestate"`
+	WinTeam           string `json:"win_team"`
+	CustomGameName    string `json:"customgamename"`
+}
+
+type Items struct {
+	Radiant struct {
+		Player0 Player `json:"player0"`
+		Player1 Player `json:"player1"`
+		Player2 Player `json:"player2"`
+		Player3 Player `json:"player3"`
+		Player4 Player `json:"player4"`
+	} `json:"team2"`
+	Dire struct {
+		Player5 Player `json:"player5"`
+		Player6 Player `json:"player6"`
+		Player7 Player `json:"player7"`
+		Player8 Player `json:"player8"`
+		Player9 Player `json:"player9"`
+	} `json:"team3"`
+}
+
+type Buildings struct {
+}
+
+type Player struct {
 	Slot1 struct {
-		AddedItem AddedItem
+		Item Item
 	} `json:"slot1"`
 	Slot2 struct {
-		AddedItem AddedItem
+		Item Item
 	} `json:"slot12"`
 	Slot3 struct {
-		AddedItem AddedItem
+		Item Item
 	} `json:"slot3"`
 	Slot4 struct {
-		AddedItem AddedItem
+		Item Item
 	} `json:"slot4"`
 	Slot5 struct {
-		AddedItem AddedItem
+		Item Item
 	} `json:"slot5"`
 	Slot6 struct {
-		AddedItem AddedItem
+		Item Item
 	} `json:"slot6"`
 }
 
-type AddedItem struct {
+type Item struct {
 	Purchaser bool `json:"purchaser"`
 	Passive   bool `json:"passive"`
 }
